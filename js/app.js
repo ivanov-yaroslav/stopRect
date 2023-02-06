e193da6389d22a401f2b77c285c8673b41afd18b
ivanov-yaroslav <98285029+ivanov-yaroslav@users.noreply.github.com>
2023-02-06 22:38:02 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -290,4 +290,216 @@
         autoHeight: false
     });
     formSubmit();
+    
+    
+    //
+    
+    const myPassMeter = passwordStrengthMeter({
+    containerElement: '#pswmeter',
+    passwordInput: '#pass1',
+    borderRadius: 3,
+    height: 5,
+    pswMinLength: 7,
+});
+
+passMessage();
+
+function passMessage() {
+    const myInput = document.getElementById('pass1');
+    const myInput2 = document.getElementById('pass2');
+    const submitButton = document.getElementById('button-submit');
+    const letter = document.getElementById('letter');
+    const number = document.getElementById('number');
+    const length = document.getElementById('length');
+
+    // When the user clicks on the password field, show the message box
+    myInput.addEventListener('focusin', e => {
+        document.querySelector('.pass-message').classList.add('show-message');
+    });
+
+    // When the user clicks outside of the password field, hide the message box
+    myInput.addEventListener('focusout', e => {
+        document.querySelector('.pass-message').classList.remove('show-message');
+        const test = /(?=.*\d)(?=.*[a-z]).{7,}/g;
+
+        if (
+            !myInput.value.match(test) &&
+            !myInput.classList.contains('_form-error') &&
+            myInput.value.length
+        ) {
+            addError(myInput, 'Not all conditions are met!');
+        }
+    });
+
+    myInput2.addEventListener('focusout', e => {});
+
+    myInput2.addEventListener('input', e => {
+        confirmPassword();
+    });
+
+    // When the user starts to type something inside the password field
+
+    myInput.addEventListener('input', e => {
+        const spaces = /\s/g;
+        confirmPassword();
+
+        if (myInput.value.match(spaces)) {
+            addError(myInput, 'This symbol is not allowed!');
+            return;
+        } else if (myInput.classList.contains('_form-error')) {
+            removeError(myInput);
+        }
+
+        const lowerCaseLetters = /[a-z]/g;
+        if (myInput.value.match(lowerCaseLetters)) {
+            letter.classList.remove('invalid');
+            letter.classList.add('valid');
+        } else {
+            letter.classList.remove('valid');
+            letter.classList.add('invalid');
+        }
+
+        // Validate numbers
+        const numbers = /[0-9]/g;
+        if (myInput.value.match(numbers)) {
+            number.classList.remove('invalid');
+            number.classList.add('valid');
+        } else {
+            number.classList.remove('valid');
+            number.classList.add('invalid');
+        }
+
+        // Validate length
+        if (myInput.value.length >= 7) {
+            length.classList.remove('invalid');
+            length.classList.add('valid');
+        } else {
+            length.classList.remove('valid');
+            length.classList.add('invalid');
+        }
+    });
+
+    function confirmPassword() {
+        const test = /(?=.*\d)(?=.*[a-z]).{7,}/g;
+        if (
+            myInput.value.match(test) &&
+            !myInput.value.match(/\s/g) &&
+            myInput.value === myInput2.value
+        ) {
+            submitButton.removeAttribute('disabled');
+            removeError(myInput2);
+        } else {
+            submitButton.setAttribute('disabled', true);
+            if (myInput2.value.length) {
+                addError(myInput2, 'Passwords must be the same and meet the requirements!');
+            } else {
+                removeError(myInput2);
+            }
+        }
+    }
+}
+
+function addError(formRequiredItem, message) {
+    formRequiredItem.classList.add('_form-error');
+    formRequiredItem.parentElement.classList.add('_form-error');
+    let inputError = formRequiredItem.parentElement.querySelector('.form__error');
+    if (inputError) formRequiredItem.parentElement.removeChild(inputError);
+    if (message) {
+        formRequiredItem.parentElement.insertAdjacentHTML(
+            'beforeend',
+            `<div class="form__error">${message}</div>`
+        );
+    }
+}
+function removeError(formRequiredItem) {
+    formRequiredItem.classList.remove('_form-error');
+    formRequiredItem.parentElement.classList.remove('_form-error');
+    if (formRequiredItem.parentElement.querySelector('.form__error')) {
+        formRequiredItem.parentElement.removeChild(
+            formRequiredItem.parentElement.querySelector('.form__error')
+        );
+    }
+}
+
+function passwordStrengthMeter(a) {
+    function b() {
+        let a = c();
+        d(a);
+    }
+    function c() {
+        let a = 0,
+            b = /(?=.*[a-z])/,
+            c = /(?=.*[0-9])/,
+            d = new RegExp('(?=.{' + j + ',})');
+        return (
+            i.match(b) && ++a,
+            i.match(c) && ++a,
+            i.match(d) && ++a,
+            0 == a && 0 < i.length && ++a,
+            a
+        );
+    }
+    function d(a) {
+        1 === a
+            ? ((g.className = 'password-strength-meter-score psms-25'),
+              k && (k.textContent = l[1] || 'Too simple'),
+              f.dispatchEvent(new Event('onScore1', { bubbles: !0 })))
+            : 2 === a
+            ? ((g.className = 'password-strength-meter-score psms-50'),
+              k && (k.textContent = l[2] || 'Simple'),
+              f.dispatchEvent(new Event('onScore2', { bubbles: !0 })))
+            : 3 === a
+            ? ((g.className = 'password-strength-meter-score psms-75'),
+              k && (k.textContent = l[3] || "That's OK"),
+              f.dispatchEvent(new Event('onScore3', { bubbles: !0 })))
+            : ((g.className = 'password-strength-meter-score'),
+              k && (k.textContent = l[0] || 'No data'),
+              f.dispatchEvent(new Event('onScore0', { bubbles: !0 })));
+    }
+    const e = document.createElement('style');
+    document.body.prepend(e),
+        (e.innerHTML = `
+    ${a.containerElement} {
+      height: ${a.height || 4}px;
+      background-color: #eee;
+      position: relative;
+      overflow: hidden;
+      border-radius: ${a.borderRadius.toString() || 2}px;
+    }
+    ${a.containerElement} .password-strength-meter-score {
+      height: inherit;
+      width: 0%;
+      transition: .3s ease-in-out;
+      background: ${a.colorScore1 || '#ff7700'};
+    }
+    ${a.containerElement} .password-strength-meter-score.psms-25 {width: 33%; background: ${
+            a.colorScore1 || '#ff7700'
+        };}
+    ${a.containerElement} .password-strength-meter-score.psms-50 {width: 66%; background: ${
+            a.colorScore2 || '#ffff00'
+        };}
+    ${a.containerElement} .password-strength-meter-score.psms-75 {width: 100%; background: ${
+            a.colorScore4 || '#00ff00'
+        };}`);
+    const f = document.getElementById(a.containerElement.slice(1));
+    f.classList.add('password-strength-meter');
+    let g = document.createElement('div');
+    g.classList.add('password-strength-meter-score'), f.appendChild(g);
+    const h = document.getElementById(a.passwordInput.slice(1));
+    let i = '';
+    h.addEventListener('keyup', function () {
+        const spaces = /\s/g;
+        if (this.value.match(spaces)) {
+            return;
+        }
+        (i = this.value), b();
+    });
+    let j = a.pswMinLength || 8,
+        k = a.showMessage ? document.getElementById(a.messageContainer.slice(1)) : null,
+        l =
+            void 0 === a.messagesList
+                ? ['No data', 'Too simple', 'Simple', "That's OK", 'Great password!']
+                : a.messagesList;
+    return k && (k.textContent = l[0] || 'No data'), { containerElement: f, getScore: c };
+}
 })();
