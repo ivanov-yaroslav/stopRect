7ea2ba4dcf4a100d594b317cf38b9fe9f72f45fb
ivanov-yaroslav <98285029+ivanov-yaroslav@users.noreply.github.com>
2023-02-08 12:58:26 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -503,3 +503,60 @@ function passwordStrengthMeter(a) {
     return k && (k.textContent = l[0] || 'No data'), { containerElement: f, getScore: c };
 }
 })();
+(() => {
+    const timer = document.querySelector('[data-timer]');
+
+    if (timer) {
+        const deadline = timer.dataset.timer;
+        setClock();
+
+        function getTimeRemainig(andTime) {
+            const t = Date.parse(andTime) - new Date();
+            const days = Math.floor(t / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+            const minutes = Math.floor((t / (1000 * 60)) % 60);
+            const seconds = Math.floor((t / 1000) % 60);
+
+            return {
+                total: t,
+                days,
+                hours,
+                minutes,
+                seconds,
+            };
+        }
+
+        function getZero(num) {
+            if (num < 10) {
+                return `0${num}`;
+            } else return num;
+        }
+
+        function setClock() {
+            const days = timer.querySelector('#days');
+            const hours = timer.querySelector('#hours');
+            const minutes = timer.querySelector('#minutes');
+            const seconds = timer.querySelector('#seconds');
+            const timeInterval = setInterval(updateClock, 1000);
+
+            updateClock();
+
+            function updateClock() {
+                const t = getTimeRemainig(deadline);
+
+                days.innerHTML = getZero(t.days);
+                hours.innerHTML = getZero(t.hours);
+                minutes.innerHTML = getZero(t.minutes);
+                seconds.innerHTML = getZero(t.seconds);
+                if (t.total <= 0) {
+                    clearInterval(timeInterval);
+
+                    days.innerHTML = 0;
+                    hours.innerHTML = 0;
+                    minutes.innerHTML = 0;
+                    seconds.innerHTML = 0;
+                }
+            }
+        }
+    }
+})();
